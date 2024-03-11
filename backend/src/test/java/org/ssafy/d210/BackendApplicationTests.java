6b5f76772c05c1ea421ebf103b075fb43ffe392e
soomin <wjsaos2081@gmail.com>
2024-03-11 13:40:21 +0900

    chore: 자동통합 환경세팅

---
+++ backend/src/test/java/org/ssafy/d210/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssafy.d210;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
